07fe60a07789a333257d9369687dcfa936f90f79
rohand575 <rohan.dhanawade97@gmail.com>
2025-09-29 19:41:55 +0200

    added react effect

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import SplashCursor from "@/components/sections/SplashCursor"
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+
+    <SplashCursor />
+
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Index />} />          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
